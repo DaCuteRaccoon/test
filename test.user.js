af8d77965234b89d24347fd18c614b9764dacb22
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-02-02 15:17:26 -0800

    Update test.user.js

---
+++ test.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @match        https://*/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
